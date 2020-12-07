6fe0a76ecdc6c8a4a137f4f34da5dd2f44875b35
shaldixi <65905068+shaldixi@users.noreply.github.com>
2020-12-07 15:48:44 +0530

    created basic structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='testrepo',
+    version='0.0.1',
+    description='My private package from github repo',
+    url='git@github.com:shaldixi/testrepo.git',
+    author='Shalini Dixit',
+    author_email='shalinidxt0@gmail.com',
+    license='unlicense',
+    packages=['testrepo'],
+    zip_safe=False
+)
